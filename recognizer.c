f9960f11d7b3979f78affe58bfab9bedee8807d5
MaJessica9 <jamma@ucdavis.edu>
2015-03-04 18:03:33 -0800

    Update recognizer.c

--- recognizer.c
+++ recognizer.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-int recognizer(float finalB1L1[200][1], float finalB1L2[200][1], float finalW1L1[200][784], float finalW1L2[200][200], float finalSoftmaxTheta[10][200], int data)
+int recognizer(float finalB1L1[200][1], float finalB1L2[200][1], float finalW1L1[200][784], float finalW1L2[200][200], float finalSoftmaxTheta[10][200], int data[784][1])
 {
-  Vdat = data;
-  Vb1 = finalW1L1*Vdat;
+  int Vb1[784][1];
+  int M;
+  
+  // Vb1 = finalW1L1*data;
+  for (i = 0; i = 784; i++)
+  {
+    
+  } // finalW1L1*data
   Vb1 = Vb1 + finalB1L1;
   Vb1 = sigmf(Vb1,[1 0]);
   Vb1 = finalW1L2*Vb1;
