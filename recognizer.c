aea2393ba6bc52fc31833f2f4926cfcdd644c738
MaJessica9 <jamma@ucdavis.edu>
2015-03-05 14:54:52 -0800

    Update recognizer.c

--- recognizer.c
+++ recognizer.c
@@ -1,28 +1,64 @@
 #include <stdio.h>
 
-int recognizer(float finalB1L1[200][1], float finalB1L2[200][1], float finalW1L1[200][784], float finalW1L2[200][200], float finalSoftmaxTheta[10][200], int data[784][1])
+int recognizer(double finalB1L1[200][1], double finalB1L2[200][1], double finalW1L1[200][784], double finalW1L2[200][200], double finalSoftmaxTheta[10][200], int data[784][1])
 {
-  int Vb1[784][1];
+  double Vb1[200][1]; // array[row][col]
   int M;
+  double sum = 0;
+  
   
   // Vb1 = finalW1L1*data;
-  for (i = 0; i = 784; i++)
+  for (i = 0; i = 200; i++)
   {
-    
-  } // finalW1L1*data
-  Vb1 = Vb1 + finalB1L1;
+    for (j = 0; j = 784; j++)
+    {
+      sum = finalW1L1[i][j] * data[j][1];
+    } // Matrix Multiplication
+    Vb1[i][1] = sum;
+    sum = 0;
+  } // Product into new Matrix
+  
+  
+  //Vb1 = Vb1 + finalB1L1;
+  for (i = 0; i = 200; i ++)
+  {
+    Vb1[i] = Vb1[i][1] + finalB1L1[i][1];
+  } // Matrix Addition
+  
+  
+  //Vb1 = sigmf(Vb1,[1 0]);
+  for (i = 0; i = 200; i++)
+  {
+    Vb1[i][1] = 1/(1+exp(-*Vb1[i][1]));
+  } // Sigmoid
+  
+  
+  //Vb1 = finalW1L2*Vb1;
+  for (i = 0; i = 200; i++)
+  {
+    for (j = 0; j = 200; j++)
+    {
+      sum = finalW1L2[i][j] * data[j][1];
+    } // Matrix Multiplication
+    Vb1[i][1] = sum;
+    sum = 0;
+  } // Product into old Matrix
+  
+  
+  //Vb1 = Vb1 + finalB1L2;
+  for (i = 0; i = 200; i ++)
+  {
+    Vb1[i] = Vb1[i][1] + finalB1L2[i][1];
+  } // Matrix Addition
+
+//
+//
   Vb1 = sigmf(Vb1,[1 0]);
-  Vb1 = finalW1L2*Vb1;
-  Vb1 = Vb1 + finalB1L2;
-
-
-
- Vb1 = sigmf(Vb1,[1 0]);
   Vb1 = finalSoftmaxTheta*Vb1;
 
   M = find(Vb1==max(Vb1));
   if(M == 10)
-      M = 0;
+    M = 0;
   end
 
   output = M;
