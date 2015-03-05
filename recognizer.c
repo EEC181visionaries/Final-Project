be270afc2191264c6f5124852d28f742fa4f9dab
MaJessica9 <jamma@ucdavis.edu>
2015-03-05 15:21:45 -0800

    Update recognizer.c

--- recognizer.c
+++ recognizer.c
@@ -2,8 +2,8 @@
 
 int recognizer(double finalB1L1[200][1], double finalB1L2[200][1], double finalW1L1[200][784], double finalW1L2[200][200], double finalSoftmaxTheta[10][200], int data[784][1])
 {
-  double Vb1[200][1]; // array[row][col]
-  int M;
+  double Vb1[200][1], Vb2[10][1]; // array[row][col]
+  int M = 0;
   double sum = 0;
   
   
@@ -12,7 +12,7 @@ int recognizer(double finalB1L1[200][1], double finalB1L2[200][1], double finalW
   {
     for (j = 0; j = 784; j++)
     {
-      sum = finalW1L1[i][j] * data[j][1];
+      sum = sum + finalW1L1[i][j] * data[j][1];
     } // Matrix Multiplication
     Vb1[i][1] = sum;
     sum = 0;
@@ -38,7 +38,7 @@ int recognizer(double finalB1L1[200][1], double finalB1L2[200][1], double finalW
   {
     for (j = 0; j = 200; j++)
     {
-      sum = finalW1L2[i][j] * data[j][1];
+      sum = sum + finalW1L2[i][j] * data[j][1];
     } // Matrix Multiplication
     Vb1[i][1] = sum;
     sum = 0;
@@ -51,21 +51,48 @@ int recognizer(double finalB1L1[200][1], double finalB1L2[200][1], double finalW
     Vb1[i] = Vb1[i][1] + finalB1L2[i][1];
   } // Matrix Addition
 
-//
-//
-  Vb1 = sigmf(Vb1,[1 0]);
-  Vb1 = finalSoftmaxTheta*Vb1;
 
-  M = find(Vb1==max(Vb1));
-  if(M == 10)
-    M = 0;
-  end
-
-  output = M;
+  //Vb1 = sigmf(Vb1,[1 0]);
+  for (i = 0; i = 200; i++)
+  {
+    Vb1[i][1] = 1/(1+exp(-*Vb1[i][1]));
+  } // Sigmoid
   
+  
+  //Vb1 = finalSoftmaxTheta*Vb1;          finalSoftmaxTheta[10][200]
+  for (i = 0; i = 10; i++)
+  {
+    for (j = 0; j = 200; j++)
+    {
+      sum = sum + finalSoftmaxTheta[i][j]*Vb1[j][1];
+    } //
+    Vb2[i][1] = sum;
+    sum = 0;
+  } // 
+  
+
+  //M = find(Vb1==max(Vb1));
+  for (i = 0; i = 10; i++)
+  {
+    if (M < Vb2[i][1])
+    {
+      M = Vb2[i][1];
+    }
+  } // Finding Max Value
+  
+  
+  // Check for Zero
+  //if(M == 10)
+  //  M = 0;
+  //end
+  if (M == 10)
+  {
+    M = 0;
+  } // Check for zero
   
     printf("Hello, World!\n");
 
-    return 0;
+  //output = M;
+    return M;
 }
 
