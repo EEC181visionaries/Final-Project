aced1556a256083623e343a5a3a23036d8a6bc64
MaJessica9 <jamma@ucdavis.edu>
2015-03-11 16:18:28 -0700

    Update read_nn.c

--- read_nn.c
+++ read_nn.c
@@ -1,6 +1,6 @@
-// read_nn.c
-// Reads in the files that were included in NN.mat
-// Files are used for the image processing neural network
+// read_nn.c - Reads in the files that were
+// included in NN.mat. Files are used for
+// the image processing neural network.
 
 #include<stdio.h>
 
@@ -12,13 +12,20 @@ double finalW1L2[200][200];
 double finalSoftmaxTheta[10][200];
 int data[784];
 
+/*
+void read_nn(void);
 // Test if it works...
 int main(void)
 {
+  int i; 
   read_nn();
-  printf("%d", finalB1L1[1]);
+  for (i = 0; i < 10; i ++)
+  {
+    printf("%le\n", finalB1L1[i]);
+  }
   return 0;
 }
+*/
 
 //
 // Reads in files for the Neural Netword
@@ -34,7 +41,7 @@ void read_nn(void)
   {
     for(j = 0; j < 784; j++)
     {
-      fscanf(fp, "%d", &(finalW1L1[i][j]));
+      fscanf(fp, "%le", &(finalW1L1[i][j]));
     }
   }
   fclose(fp);
@@ -45,7 +52,7 @@ void read_nn(void)
   {
     for(j = 0; j < 200; j++)
     {
-      fscanf(fp, "%d", &(finalW1L2[i][j]));
+      fscanf(fp, "%le", &(finalW1L2[i][j]));
     }
   }
   fclose(fp);
@@ -56,7 +63,7 @@ void read_nn(void)
   {
     for(j = 0; j < 200; j++)
     {
-      fscanf(fp, "%d", &(finalSoftmaxTheta[i][j]));
+      fscanf(fp, "%le", &(finalSoftmaxTheta[i][j]));
     }
   }
   fclose(fp);
@@ -65,7 +72,7 @@ void read_nn(void)
   fp = fopen("finalB1L1.csv","r");
   for(i = 0; i < 200; i++)
   {
-    fscanf(fp, "%d", &(finalB1L1[i]));
+    fscanf(fp, "%le", &(finalB1L1[i]));
   }
   fclose(fp);
 	
@@ -73,7 +80,7 @@ void read_nn(void)
   fp = fopen("finalB1L2.csv","r");
   for(i = 0; i < 200; i++)
   {
-    fscanf(fp, "%d", &(finalB1L2[i]));
+    fscanf(fp, "%le", &(finalB1L2[i]));
   }
   fclose(fp);
 
@@ -84,4 +91,5 @@ void read_nn(void)
     fscanf(fp, "%d", &(data[i]));
   }
   fclose(fp);	
+
 }// read_nn(void)
