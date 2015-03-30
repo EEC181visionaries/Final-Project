32ff83baa12f47d432637b341609b2e09154f75c
MaJessica9 <jamma@ucdavis.edu>
2015-03-30 13:52:17 -0700

    Update test.c

--- test.c
+++ test.c
@@ -1,31 +1,37 @@
 // test.c - Reads in a photo and prints out data
 
 // Libraries
-#include<stdlib.h>
-#include<stdio.h>
-#include<math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
-#include"finalB1L1.c"
-#include"finalB1L2.c"
-#include"finalSoftmaxTheta.c"
-#include"finalW1L1.c"
-#include"finalW1L2.c"
+#include "finalB1L1.c"
+#include "finalB1L2.c"
+#include "finalSoftmaxTheta.c"
+#include "finalW1L1.c"
+#include "finalW1L2.c"
 
 // Definitions
 #define WIDTH 640
 #define HEIGHT 480
-#define DDR3_ADDR 0x00000000	// Address of SDRAM
+#define BW_LEVEL  350
 
-// Declare global variables
-// Reading in files
-//long double finalB1L1[200];
-//long double finalB1L2[200];
-//long double finalW1L1[200][784];
-//long double finalW1L2[200][200];
-//long double finalSoftmaxTheta[10][200];
+// Registers
+#define READ_IN_ADDR  0xFF200090
+#define READ_OUT_ADDR   0xFF200080
+#define VGA_DATA1 0xFF200070
+#define VGA_DATA2 0xFF200060
+#define SDRAM_DATA1 0xFF200050
+#define SDRAM_DATA2 0xFF200040
+#define VGA_CLK_IN  0xFF200030
+#define CAM_START 0xFF200020
+#define SOURCE_SELECT 0xFF200010
+#define CONTROLLING_CLK 0xFF200000
+#define TEST 0xFF200100
+#define DDR3_ADDR 0x00100000
 
 // Computing ROI and Separate Images
-double image[HEIGHT][WIDTH];
+//int image[HEIGHT][WIDTH];
 int w, x, y, v, lt, lb, rt, rb;
 int black_white[HEIGHT][WIDTH];
 int roi[HEIGHT][WIDTH];
@@ -34,23 +40,154 @@ int data[784];
 int size_x = 0, size_y = 0;
 
 // List of functions
-void read_pic(void);
-//void read_nn(void);
+//void read_pic(void);
 void region(void);
 void resize(void);
 int recognizer(void);
 
 int main(void)
 {
+  volatile int * (cam_start) = (int *) CAM_START; // Output
+  volatile int * (sdram_data1) = (int *) SDRAM_DATA1; //Input
+  volatile int * (sdram_data2) = (int *) SDRAM_DATA2; //Input
+  volatile int * (sdram_read) = (int *) READ_OUT_ADDR;  //Output
+  volatile int * (read_good) = (int *) VGA_CLK_IN;  //Input
+  volatile int * (vga_read) = (int *) READ_IN_ADDR; //Input
+  volatile int * (vga_data1) = (int *) VGA_DATA1; //Output
+  volatile int * (vga_data2) = (int *) VGA_DATA2; //Output
+  volatile int *(clock_test) = (int *) TEST;
+  volatile int * (ddr3) = (int *) DDR3_ADDR;
+  volatile int * write_block;
+  volatile int *(clock_select) = (int *) SOURCE_SELECT;
+  volatile int *(clock_gen) = (int *) CONTROLLING_CLK;
   int M;
-  //read_nn();  
-  read_pic();
+  write_block = DDR3_ADDR;
+  int red = 0;
+  int green = 0;
+  int green_h = 0;
+  int green_l = 0;
+  int blue = 0;
+  int gray = 0;
+  int i = 0;
+  int j = 0;
+  int k = 0;
+  int L = 0;
+  int snapshot = 0;
+  int test[10000] = {0};
+  *vga_data1 = 1;
+  *clock_select = 0;
+  int write_data = 0;
+  int written = 0;
+
+
+  *cam_start = 1;
+  printf("Press enter to start\n");
+
+  // fill blocks with known value for debugging. Not needed in the end
+ /*  for (j = 0; j < (307200); j = j+1) // 640x480
+   {
+     *(write_block) = 0x7FFF7FFF;//*(sdram_data1);
+        write_block++;
+   }
+   */
+   write_block = DDR3_ADDR;
+
+   fflush(stdin);
+   getchar();
+   fflush(stdin);
+
+
+
+    // delay before capture
+    for (i = 0; i < 30000; i++)
+    {
+    }
+      snapshot = 1;
+    if (snapshot)
+    {
+      *cam_start = 0; // pause camera
+      *clock_select = 1;  // choose custom clock from hps
+      *vga_data1 = 0; // reset sdram
+      *vga_data1 = 1; 
+      *sdram_read = 1;  // set read request to high
+
+      // clear out first horizontal row, it is all black
+      for (k = 0; k < 643; k = k+1)
+      {
+        *clock_gen = 1; // generate 4 clock cycles to move slower clock 1 cycle
+        *clock_gen = 0;
+        *clock_gen = 1;
+        *clock_gen = 0;
+        *clock_gen = 1;
+        *clock_gen = 0;
+        *clock_gen = 1;
+        *clock_gen = 0;
+      }
+
+      // begin reading in data
+      for (j = 0; j < 480; j = j+1)
+      {
+      for (k = 0; k < 640; k = k+1)
+      {
+        for (L = 0; L < 4; L = L+1)
+        {
+          *clock_gen = 1; // generate 4 clock cycles, checking each cycle
+          if (!written)
+          {
+            if (*read_good) // take in data from verilog to read block (not sure if needed)
+            {
+              write_data = *(sdram_data1);
+              if (write_data < BW_LEVEL)  // write black or white
+              {
+                //*write_block = 0;
+                black_white[j][k] = 0;
+              }
+              else
+              {
+                //*write_block = 1;
+                black_white[j][k] = 1;
+              }
+              //write_block++;
+              written = 1;
+            }
+          }
+          *clock_gen = 0;
+        }
+        written = 0;
+      }
+      // write blocks to separate each row to debug in memory
+      /*
+      *(write_block) = 0x7FFF7FFF;
+      write_block++;
+      *(write_block) = 0x7FFF7FFF;
+      write_block++;
+      *(write_block) = 0x7FFF7FFF;
+      write_block++;
+      *(write_block) = 0x7FFF7FFF;
+      write_block++;
+      */
+      *sdram_read = 0;
+      *sdram_read = 1;
+      }
+      *sdram_read = 0;
+      //*sdram_read = *vga_read;
+      *cam_start = 1;
+      *clock_select = 0;
+      snapshot = 0;
+      printf("Done\n");
+    }
+
+  *(sdram_read) = 0;
+
+
+
 
   region();
   resize();
   M = recognizer();
-  printf("Guessed %d\n",M);
+  printf("Guessed %d\n\n",M);
 
+/*
   FILE *f = fopen("image.csv", "w");
   if (f == NULL)
   {
@@ -73,9 +210,10 @@ int main(void)
     }//for j
   }//for i
   fclose(f);
+  */
 
 
-  
+
   return 0;
 }
 
@@ -83,6 +221,7 @@ int main(void)
 //
 // read_pic - Reads in photo data
 //
+/*
 void read_pic(void)
 {
   FILE* fp;
@@ -100,68 +239,7 @@ void read_pic(void)
   fclose(fp);
 
 }
-
-
-//
-// Reads in files for the Neural Netword
-//
-void read_nn(void)
-{
-    FILE* fp;
-  int i,j;
-
-  // Open finalW1L1.csv
-  fp = fopen("finalW1L1.csv","r");
-  for(i = 0; i < 200; i++)
-  {
-    for(j = 0; j < 784; j++)
-    {
-      fscanf(fp, "%Le", &(finalW1L1[i][j]));
-    }//for j
-  }//for i
-  fclose(fp);
-
-  // Open finalW1L2.csv	
-  fp = fopen("finalW1L2.csv","r");
-  for(i = 0; i < 200; i++)
-  {
-    for(j = 0; j < 200; j++)
-    {
-      fscanf(fp, "%Le", &(finalW1L2[i][j]));
-    }//for j
-  }//for i
-  fclose(fp);
-
-  // Open finalSoftmaxTheta.csv	
-  fp = fopen("finalSoftmaxTheta.csv","r");
-  for(i = 0; i < 10; i++)
-  {
-    for(j = 0; j < 200; j++)
-    {
-      fscanf(fp, "%Le", &(finalSoftmaxTheta[i][j]));
-    }//for j
-  }//for i
-  fclose(fp);
-
-  // Open finalB1L1.csv
-  fp = fopen("finalB1L1.csv","r");
-  for(i = 0; i < 200; i++)
-  {
-    fscanf(fp, "%Le", &(finalB1L1[i]));
-  }//for i
-  fclose(fp);
-	
-  // Open finalB1L2.csv
-  fp = fopen("finalB1L2.csv","r");
-  for(i = 0; i < 200; i++)
-  {
-    fscanf(fp, "%Le", &(finalB1L2[i]));
-  }//for i
-  fclose(fp);
-
-}// read_nn(void)
-
-
+*/
 
 void region(void)
 {
@@ -247,6 +325,7 @@ void region(void)
 
 // Print LROI
   printf("%d, %d, %d, %d\n", x, y, v, w);
+  /*
   FILE *fp = fopen("lroi.csv", "w");
 
   if (fp == NULL)
@@ -270,6 +349,7 @@ void region(void)
   }
   fclose(fp);
 // end print LROI
+*/
 
 
 // Get new x
@@ -373,6 +453,7 @@ void region(void)
   size_y = tempy;
   //printf("%d, %d\n", size_x, size_y);
 
+/*
 // Prints out region of interest
   printf("%d, %d, %d, %d\n", x, y, v, w);
   fp = fopen("roi.csv", "w");
@@ -397,6 +478,7 @@ void region(void)
     }
   }
   fclose(fp);
+  */
 }//region()
 
 
@@ -437,6 +519,7 @@ void resize(void){
 		
 	}
 
+/*
 	  fp = fopen("resize.csv", "w");
 
   if (fp == NULL)
@@ -462,7 +545,7 @@ void resize(void){
     }
   }
   fclose(fp);
-
+*/
 }
 
 int recognizer(void)
@@ -562,7 +645,6 @@ int recognizer(void)
   //output = M;
     return M;
 }
-
 
 
 
