7680b6c6aa39395e5f152518db93e275c13dadbf
ChanWolfLu <chan_lu2010@hotmail.com>
2015-04-15 14:29:08 -0700

    Update SDRv3compilable.c

--- SDRv3compilable.c
+++ SDRv3compilable.c
@@ -972,3 +972,24 @@ void digit_separate2(int num_row, int num_col, int **roi)
 } // digit_separate2
 
 
+static inline unsigned int getCycles ()
+{
+unsigned int cycleCount;
+// Read CCNT register
+asm volatile ("MRC p15, 0, %0, c9, c13, 0\t\n": "=r"(cycleCount));
+return cycleCount;
+}
+static inline void initCounters ()
+{
+// Enable user access to performance counter
+asm volatile ("MCR p15, 0, %0, C9, C14, 0\t\n" :: "r"(1));
+// Reset all counters to zero
+int MCRP15ResetAll = 23;
+asm volatile ("MCR p15, 0, %0, c9, c12, 0\t\n" :: "r"(MCRP15ResetAll));
+// Enable all counters:
+asm volatile ("MCR p15, 0, %0, c9, c12, 1\t\n" :: "r"(0x8000000f));
+// Disable counter interrupts
+asm volatile ("MCR p15, 0, %0, C9, C14, 2\t\n" :: "r"(0x8000000f));
+// Clear overflows:
+asm volatile ("MCR p15, 0, %0, c9, c12, 3\t\n" :: "r"(0x8000000f));
+}
